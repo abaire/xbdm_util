6aeffeb2df024b184c43f8b6c672e3ec0f003e00
Erik Abair <erik.abair@gmail.com>
2021-10-16 18:26:19 -0700

    Adds class to encapsulate NAP discovery.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""See https://xboxdevwiki.net/Xbox_Debug_Monitor"""
+
+import collections
 import socket
 import struct
 import sys
@@ -10,10 +13,12 @@ XBDM_PORT_NAT = 1731
 XBDM_PORT = 731
 
 # Interval between NAP discovery packets.
-DISCOVERY_BROADCAST_INTERVAL_SECONDS = 0.5
+DISCOVERY_BROADCAST_INTERVAL_SECONDS = 0.75
 
 
 class NAPPacket:
+    """Models an NAP discovery packet."""
+
     TYPE_INVALID = 0
     TYPE_LOOKUP = 1
     TYPE_REPLY = 2
@@ -36,45 +41,86 @@ class NAPPacket:
         return 2 + len(self.name)
 
 
-def discovery_broadcast_thread():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+class XBOXDiscoverer:
+    """Broadcasts and handles responses to NAP discovery packets."""
 
-    discovery_packet = NAPPacket.build_discovery_packet()
+    def __init__(self, listen_ip="", listen_port=XBDM_PORT):
+        self.listen_ip = listen_ip
+        self.listen_port = listen_port
+        self._xbox_registry_lock = threading.RLock()
+        self._xbox_name_registry = collections.defaultdict(set)
+        self._xbox_ip_registry = {}
 
-    while True:
-        time.sleep(DISCOVERY_BROADCAST_INTERVAL_SECONDS)
-        bytes_sent = sock.sendto(discovery_packet.serialize(), ("<broadcast>", XBDM_PORT))
-        if (bytes_sent != 2):
-            print("ERROR: Failed to send discovery packet.")
+        self._discovery_thread = threading.Thread(
+            target=XBOXDiscoverer.discovery_broadcast_thread,
+            name="DiscoveryBroadcast",
+            args=(self,),
+        )
+        self._recv_thread = threading.Thread(
+            target=XBOXDiscoverer.discovery_recv_thread,
+            name="DiscoveryRecv",
+            args=(self,),
+        )
 
+    def start(self):
+        self._discovery_thread.start()
+        self._recv_thread.start()
 
-def discovery_recv_thread(listen_ip):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((listen_ip, XBDM_PORT))
+    @staticmethod
+    def discovery_broadcast_thread(discoverer):
+        print("Broadcast thread started")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-    reply_packet = NAPPacket()
+        discovery_packet = NAPPacket.build_discovery_packet()
 
-    while True:
-        data, addr = sock.recvfrom(277)
+        while True:
+            time.sleep(DISCOVERY_BROADCAST_INTERVAL_SECONDS)
+            bytes_sent = sock.sendto(
+                discovery_packet.serialize(), ("<broadcast>", discoverer.listen_port)
+            )
+            if bytes_sent != 2:
+                print("ERROR: Failed to send discovery packet.")
 
-        if (len(data) < 2):
-            print("Ignoring unexpected data")
-            continue
+    @staticmethod
+    def discovery_recv_thread(discoverer):
+        print("Recv thread started")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((discoverer.listen_ip, discoverer.listen_port))
 
-        bytes_consumed = reply_packet.deserialize(data)
-        if (len(data) != bytes_consumed or reply_packet.type != reply_packet.TYPE_REPLY):
-            print(f"Received unexpected non-NAP packet with type: {reply_packet.TYPE_REPLY}")
-            continue
+        reply_packet = NAPPacket()
 
-        print("XBOX discovered at {addr}")
+        while True:
+            data, addr = sock.recvfrom(277)
+
+            if len(data) < 2:
+                print("Ignoring unexpected data")
+                continue
+
+            bytes_consumed = reply_packet.deserialize(data)
+            if len(data) != bytes_consumed:
+                print(
+                    f"Received unexpected non-NAP packet with type: {reply_packet.TYPE_REPLY} from {addr}"
+                )
+                continue
+
+            if reply_packet.type != reply_packet.TYPE_REPLY:
+                # Ignore any lookups or wildcard requests.
+                continue
+
+            print("XBOX discovered at {addr}")
+            with discoverer._xbox_registry_lock:
+                discoverer._xbox_name_registry[reply_packet.name].add(addr)
+                discoverer._xbox_ip_registry[addr] = reply_packet.name
 
 
 def main():
+    print("Startup")
+    discoverer = XBOXDiscoverer()
+    discoverer.start()
 
-    threading.Thread(target=discovery_broadcast_thread)
-    threading.Thread(target=discovery_recv_thread, args=('',))
-
+    while True:
+        time.sleep(1000)
     # sock = socket.socket(socket.AF_INET, # Internet
     #                      socket.SOCK_DGRAM) # UDP
     # sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
